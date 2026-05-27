85f953e14ca46c89a43316e32d67de0d0c1b97ad
Adam Daley <admdly@users.noreply.github.com>
2026-05-27 01:19:09 +0100

    Remove redirects block from astro.config.mjs (#36)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,55 +6,6 @@ import markdoc from '@astrojs/markdoc';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.fossbilling.org',
-  redirects: {
-    // fossbilling.org docs redirects
-    '/maintaining-fossbilling/updating': '/maintenance/updating/',
-    '/maintaining-fossbilling/admin-manual-reset':
-      '/maintenance/admin-manual-reset/',
-    '/changelog': '/maintenance/changelog/',
-    '/customizing-fossbilling/config': '/admin-guide/config/',
-    '/customizing-fossbilling/localization': '/admin-guide/localization/',
-    '/customizing-fossbilling/company': '/admin-guide/company/',
-    '/customizing-fossbilling/invoice-pdf': '/admin-guide/invoice-pdf/',
-    '/customizing-fossbilling/email-templates': '/admin-guide/email-templates/',
-    '/developing-fossbilling/api': '/extensions-and-development/api/',
-    '/developing-fossbilling/guides/creating-a-module':
-      '/extensions-and-development/guides/creating-a-module/',
-    '/developing-fossbilling/guides/creating-a-payment-gateway':
-      '/extensions-and-development/guides/creating-a-payment-gateway/',
-    '/developing-fossbilling/guides/custom-gateway':
-      '/admin-guide/custom-payment-gateway/',
-    '/developing-fossbilling/guides/creating-a-registrar-integration':
-      '/extensions-and-development/guides/creating-a-registrar-integration/',
-    '/developing-fossbilling/guides/creating-a-server-manager':
-      '/extensions-and-development/guides/creating-a-server-manager/',
-    '/developing-fossbilling/guides/creating-a-theme':
-      '/extensions-and-development/guides/creating-a-theme/',
-    '/developing-fossbilling/code-of-conduct':
-      'https://github.com/FOSSBilling/FOSSBilling/blob/main/CODE_OF_CONDUCT.md',
-    '/developing-fossbilling/event-hooks':
-      '/extensions-and-development/event-hooks/',
-    '/developing-fossbilling/file-structure':
-      '/extensions-and-development/file-structure/',
-    '/developing-fossbilling/javascript':
-      '/extensions-and-development/javascript/',
-    '/developing-fossbilling/twig-filters':
-      '/extensions-and-development/twig-filters/',
-    '/extensions': '/extensions-and-development/extensions/',
-    '/faq': '/support/faq/',
-    '/faq/error-reporting': '/maintenance/error-reporting/',
-    '/faq/features': '/support/features/',
-    '/product-types/apikeys': '/admin-guide/product-types/apikeys/',
-    '/product-types/domains': '/admin-guide/product-types/domains/',
-    '/product-types/downloadable': '/admin-guide/product-types/downloadable/',
-    '/product-types/hosting': '/admin-guide/product-types/hosting/',
-    '/product-types/license': '/admin-guide/product-types/license/',
-    '/troubleshooting': '/maintenance/troubleshooting/',
-    '/server-managers/cwp': '/admin-guide/server-managers/cwp/',
-    '/server-managers/hestiacp': '/admin-guide/server-managers/hestiacp/',
-    '/server-managers/others': '/admin-guide/server-managers/others/',
-    '/server-managers/whm': '/admin-guide/server-managers/whm/',
-  },
   integrations: [
     starlight({
       title: 'FOSSBilling Docs',
